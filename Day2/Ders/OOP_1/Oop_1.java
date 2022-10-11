b6185b81fa398bf52d4f9f567d77a069e1c6083b
oncado86 <oncado@gmail.com>
2022-10-11 19:18:51 +0300

    update

--- Day2/Ders/OOP_1/Oop_1.java
+++ Day2/Ders/OOP_1/Oop_1.java
@@ -1,5 +1,7 @@
 package Ders.OOP_1;
 
+import java.util.ArrayList;
+
 public class Oop_1 {
     /**
      * @author <a href="https://github.com/oncado86">OnCaDo</a>
@@ -10,11 +12,46 @@ public class Oop_1 {
      **          (NYP: Nesne Yönelimli Programlama)
      */
     public static void main(String[] args) {
-        Product product = new Product();
-        product.name = "Kahve Makinesi";
-        product.unitPrice = 15;
+        // String mesaj = "OnCaDo";
 
-        System.out.println(product.name);
+        Product product1 = new Product();
+
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setUnitPrice(7500);
+        product1.setDiscount(7);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("image1.jpg");
+
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setUnitPrice(6500);
+        product2.setDiscount(8);
+        product2.setUnitsInStock(2);
+        product2.setImageUrl("image2.jpg");
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setUnitPrice(4500);
+        product3.setDiscount(9);
+        product3.setUnitsInStock(4);
+        product3.setImageUrl("image3.jpg");
+
+        Product[] products = { product1, product2, product3 };
+
+        ArrayList<Product> aProducts = new ArrayList<Product>();
+        aProducts.add(product1);
+        aProducts.add(product2);
+        aProducts.add(product3);
+
+        System.out.println("<ul>");
+        for (Product product : products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
+
+        for (Product aProduct : aProducts) {
+            System.out.println(aProduct.getName());
+        }
+
     }
-        
 }
--- Day2/Ders/OOP_1/Product.java
+++ Day2/Ders/OOP_1/Product.java
@@ -10,10 +10,50 @@ package Ders.OOP_1;
  **          Sınıflar şablon olarak kullanılabilir.
  */
 public class Product { // ? Urün
-    String name; // ? UrünAdi
-    double unitPrice; // ? birimFiyati
-    double discount; // ? indirim oranı
-    String imageUrl; // ? urunResimAdresi
-    int unitsInStock; // ? stokMiktari
+    private String name; // ? UrünAdi
+    private double unitPrice; // ? birimFiyati
+    private double discount; // ? indirim oranı
+    private String imageUrl; // ? urunResimAdresi
+    private int unitsInStock; // ? stokMiktari
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
 
 }
