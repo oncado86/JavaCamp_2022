7e503afd0dc3c7a325e5b17297d3a82e2058bb14
oncado86 <oncado@gmail.com>
2022-10-14 00:07:49 +0300

    update

---
+++ Day3/Homeworks/h01_OOP/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @version 1.0
+ * @author <a href="https://github.com/oncado86">OnCaDo</a>
+ * @see * JAVA Yazılım Geliştirici Kampı 2022
+ * @category JAVA
+ * 
+ ** @apiNote Çok katmanlı mimaride Java projesi.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
