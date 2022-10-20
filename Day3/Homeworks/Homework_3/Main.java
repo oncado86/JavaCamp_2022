b6380dc5d83e9e83c4dd1ac1f5e80278ce313cc6
oncado86 <oncado@gmail.com>
2022-10-20 23:39:06 +0300

    update

--- Day3/Homeworks/Homework_3/Main.java
+++ Day3/Homeworks/Homework_3/Main.java
@@ -2,6 +2,7 @@ package Homeworks.Homework_3;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import Homeworks.Homework_3.business.BaseManager;
 import Homeworks.Homework_3.core.logger.EmailLogger;
@@ -18,49 +19,49 @@ import Homeworks.Homework_3.entities.Instructor;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        // definitions --------------------------------
+        public static void main(String[] args) {
+                // definitions ---------------------------------
 
-        List<ILoggerBase> loggers = Arrays.asList(
-                new FileLogger(),
-                new EmailLogger());
+                List<ILoggerBase> loggers = Arrays.asList(
+                                new FileLogger(),
+                                new EmailLogger());
 
-        List<IEntitie> baseCategories = Arrays.asList(
-                new Category(1, "Programming"));
+                BaseManager managerJDBC = new BaseManager(new JdbcDatabaseDal(), loggers);
 
-        List<IEntitie> baseCourses = Arrays.asList(
-                new Course(1, "Java Kampı", "Java Yazılım Geliştirici Kampı", new Category(3, "Programming"),
-                        new Instructor(0, "Engin", "Demiroğ")));
+                List<IEntitie> baseCategories = Arrays.asList(
+                                new Category(0, "Programlama"));
 
-        List<IEntitie> baseInstructor = Arrays.asList(
-                new Instructor(1, "Engin", "Demiroğ"));
+                List<IEntitie> baseInstructor = Arrays.asList(
+                                new Instructor(0, "Engin", "Demiroğ"));
 
-        IEntitie kategori1 = new Category(1, "Kategori1");
-        IEntitie kategori2 = new Category(2, "Programming");
+                List<IEntitie> baseCourses = Arrays.asList(
+                                new Course(0, "Java Kampı", "Java Yazılım Geliştirici Kampı",
+                                                baseCategories.get(0), baseInstructor.get(0)));
 
-        IEntitie kurs1 = new Course(1, "C#", "C# Kampı", kategori1, new Instructor(0, "OnCaDo", "OnCaDo"));
-        IEntitie kurs2 = new Course(2, "Java Kampı", "Java Yazılım Geliştirici Kampı", kategori2,
-                new Instructor(0, "OnCaDo", "OnCaDo"));
+                IEntitie kategori1 = new Category(1, "Kategori 1");
+                IEntitie kategori2 = new Category(2, "Programlama");
 
-        IEntitie egitmen1 = new Instructor(1, "OnCaDo", "OnCaDo");
-        IEntitie egitmen2 = new Instructor(1, "Engin", "Demiroğ");
+                IEntitie egitmen1 = new Instructor(1, "OnCaDo", "OnCaDo");
+                IEntitie egitmen2 = new Instructor(2, "Engin", "Demiroğ");
 
-        BaseManager managerJdbc = new BaseManager(new JdbcDatabaseDal(), loggers);
+                IEntitie kurs1 = new Course(1, "Python", "Python Kampı", kategori1, egitmen1);
+                IEntitie kurs2 = new Course(2, "Java Kampı", "Java Yazılım Geliştirici Kampı", kategori2, egitmen2);
 
-        // ----------------------------------------------------------------
+                // tests -------------------------------------------
 
-        // category
-        System.out.println("--------------------------------");
-        managerJdbc.add(kategori1, baseCategories);
-        managerJdbc.add(kategori2, baseCategories);
-        System.out.println("--------------------------------");
-        // course
-        managerJdbc.add(kurs1, baseCourses);
-        managerJdbc.add(kurs2, baseCourses);
-        System.out.println("--------------------------------");
-        // instructor
-        managerJdbc.add(egitmen1, baseInstructor);
-        managerJdbc.add(egitmen2, baseInstructor);
-        System.out.println("--------------------------------");
-    }
+                System.out.println("--------------------------------");
+                // category
+                managerJDBC.add(kategori1, baseCategories);
+                managerJDBC.add(kategori2, baseCategories);
+                System.out.println("--------------------------------");
+                // course
+                managerJDBC.add(kurs1, baseCourses);
+                managerJDBC.add(kurs2, baseCourses);
+                System.out.println("--------------------------------");
+                // instructor
+                managerJDBC.add(egitmen1, baseInstructor);
+                managerJDBC.add(egitmen2, baseInstructor);
+                System.out.println("--------------------------------");
+
+        }
 }
--- Day3/Homeworks/Homework_3/entities/Course.java
+++ Day3/Homeworks/Homework_3/entities/Course.java
@@ -4,18 +4,27 @@ public class Course implements IEntitie{
     private int id;
     private String name;
     private String description;
-    private IEntitie entitie;
-    private Instructor instructor;
+    private IEntitie category;
+    private IEntitie instructor;
+    // private Instructor instructor;
 
     public Course() {
     }
 
     // Course(int, String, String, IEntitie, Instructor)
-    public Course(int id, String name, String description, IEntitie entitie, Instructor instructor) {
+    // public Course(int id, String name, String description, IEntitie entitie, Instructor instructor) {
+    //     this.id = id;
+    //     this.name = name;
+    //     this.description = description;
+    //     this.entitie = entitie;
+    //     this.instructor = instructor;
+    // }
+
+    public Course(int id, String name, String description, IEntitie category, IEntitie instructor) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.entitie = entitie;
+        this.category = category;
         this.instructor = instructor;
     }
 
@@ -43,20 +52,28 @@ public class Course implements IEntitie{
         this.description = description;
     }
 
-    public IEntitie getCourse() {
-        return entitie;
+    public IEntitie getCategory() {
+        return category;
     }
 
-    public void setCourse(IEntitie entitie) {
-        this.entitie = entitie;
+    public void setCategory(IEntitie category) {
+        this.category = category;
     }
 
-    public Instructor getInstructor() {
+    public IEntitie getInstructor() {
         return instructor;
     }
 
-    public void setInstructor(Instructor instructor) {
+    public void setInstructor(IEntitie instructor) {
         this.instructor = instructor;
     }
+
+    // public Instructor getInstructor() {
+    //     return instructor;
+    // }
+
+    // public void setInstructor(Instructor instructor) {
+    //     this.instructor = instructor;
+    // }
 
 }
